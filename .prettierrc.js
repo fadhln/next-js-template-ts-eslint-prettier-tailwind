099d63aa17c8a4f0af74ffa74e98756cb8c2ea1e
brandon <fadhlan1337@gmail.com>
2022-07-28 21:14:20 +0700

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSameLine: false,
+  bracketSpacing: true,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// Tailwind config for Tailwind CSS v3.0+
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
